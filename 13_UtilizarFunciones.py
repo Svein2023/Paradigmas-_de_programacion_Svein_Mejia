835042d5f683d39f51cf86e9dc130137244afc1a
Svein2023 <143757818+Svein2023@users.noreply.github.com>
2024-01-12 21:10:38 -0600

    python 13

---
+++ 13_UtilizarFunciones.py
@@ -0,0 +1,15 @@
+#===================================
+# Svein Samuel Mejia Zavala
+# Matematica algoritmica 
+# Paradigmas de Programacion
+# ESFM IPN Octubre 2023
+#==================================
+
+#===================================================
+# De funciones.py traer las funciones suma y promedio
+#=====================================================
+from funciones import suma,promedio
+print(suma(100,200))
+print(promedio(100,200))
+
+
